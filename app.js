f248274fbf3e31487b1b6566d88963e940132094
dylan <dylan@shariot.com>
2022-06-22 14:45:56 +0800

    update response message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send("Shariot testing DevOps");
+	res.send("Shariot testing DevOps! Yoyoyo!!!");
 });
  
 const port = process.env.port || 3000;
