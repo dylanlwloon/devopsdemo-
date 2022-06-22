10dacf8478e2d3901a37b7484b27a8a4b1afa6a3
dylan <dylan@shariot.com>
2022-06-22 14:36:28 +0800

    Remove extra bracket

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)) => {
+app.get('/', (req, res) => {
 	res.send("Shariot testing DevOps");
 });
-
+ 
 const port = process.env.port || 3000;
 
 app.listen(port, () => {
